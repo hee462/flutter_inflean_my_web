e3279c9ba294845e2b1e79d10acc40d17aee8785
hee462 <a01080809887@gmail.com>
2024-04-07 19:19:59 +0900

    feat : ERR_CLEARTEXT_NOT_PERMITTED 에러 해결법

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,15 +1,60 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final WebViewController _webViewController = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(Uri.parse('https://flutter.dev'));
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: Text('나만에 웹 브라우저'),
+        title: const Text('나만의 웹브라우저'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              _webViewController.loadRequest(Uri.parse(value));
+            },
+            itemBuilder: (context) => [
+              const PopupMenuItem<String>(
+                value: 'https://www.google.com',
+                child: Text('구글'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'https://www.m.naver.com/',
+                child: Text('네이버'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'https://www.kakao.com',
+                child: Text('카카오'),
+              ),
+            ],
+          ),
+        ],
       ),
-
+      body: PopScope(
+        canPop: false,
+        onPopInvoked: (didPop) async {
+          if (await _webViewController.canGoBack()) {
+            await _webViewController.goBack();
+          }
+        },
+        child: WebViewWidget(controller: _webViewController),
+      ),
     );
   }
 }
